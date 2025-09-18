ebfbac1f117a1e227afe780061062289c917ef4d
Jagath Kiran <135314273+jagathkiran@users.noreply.github.com>
2025-09-18 23:48:03 +0530

    122 best time to buy or sell stocks ii

---
+++ leetcode/122.c
@@ -0,0 +1,13 @@
+int maxProfit(int *prices, int pricesSize) {
+  int total_profit = 0, profit = 0, buy = 0;
+  for (int i = 1; i < pricesSize; i++) {
+    if (i != buy) {
+      profit = prices[i] - prices[buy];
+      if (profit > 0) {
+        total_profit += profit;
+      }
+      buy = i;
+    }
+  }
+  return total_profit;
+}
